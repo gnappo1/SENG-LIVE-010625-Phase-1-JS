3d1256d7e1b769d78605df2e3279113ae3804cc7
Matteo Piccini <gnappo.1@live.it>
2025-01-06 10:04:24 -0800

    add starter code for lecture

---
+++ 00_JS_Fundamentals/index.js
@@ -0,0 +1,44 @@
+//! JS Fundamentals Part 1
+//"use strict" // use to enforce a stricter set of rules and best-practices
+
+
+//TODO Commenting Code
+// Toggle comments using cmd + ? (mac) OR cntr + ? (pc)
+// Single Line Comments
+/*
+    Multi Line Comments
+*/
+
+//TODO Console Logging
+// console.log("test")
+// console.warn("warn")
+// console.error("error")
+
+//TODO Debugging
+//! VSC debugger, play button with the little bug
+//! Add a breakpoint by clicking on the left of the line number
+//! or use the debugger keyword when working in the browser
+
+//TODO Variables
+
+//TODO Data Types
+//! check in REPL using typeof -> typeof true
+
+//TODO Truthiness and Falseness
+
+
+//TODO String Interpolation VS Concatenation
+
+
+//TODO Conditional Statements
+// 1. if/else if/else
+
+// 2. Switch Statements
+
+//TODO Ternary Expressions
+// condition ? do something if truthy : do something if falsey
+
+//TODO Loops
+// 1. While
+
+// 2. For loop

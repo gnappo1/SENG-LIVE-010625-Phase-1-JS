a1c8ef5b3d516ce35c4fbda1902d552be2ad5638
Matteo Piccini <gnappo.1@live.it>
2025-01-06 12:24:44 -0800

    end of lecture code

--- 00_JS_Fundamentals/index.js
+++ 00_JS_Fundamentals/index.js
@@ -19,24 +19,65 @@
 //! Add a breakpoint by clicking on the left of the line number
 //! or use the debugger keyword when working in the browser
 
-//TODO Variables
-
 //TODO Data Types
 //! check in REPL using typeof -> typeof true
+//! Primitive
+// number
+// string
+// boolean (true, false)
+// null
+// undefined
+// symbol
+
+//! Structural/Object
+// object
+
+//! Special 
+// function
 
 //TODO Truthiness and Falseness
+// false, null, 0, -0, 0n, "", undefined, NaN
 
+//TODO Variables
+// let -> redeclaration is NOT possible; reassignment is possible
+// const -> redeclaration is NOT possible; reassignment is NOT possible -> it does NOT mean it's CONSTANT because if I use arrays or objects I can modify the content stored in that memory location BUT NOT CHANGE the memory location itself
+// var -> redeclaration is possible; reassignment is possible
+
+let favoriteFood = "mac & cheese"
+const favoriteDrink = "coffee"
+var favoriteDessert = "brownies"
+name = "super evil!" // implicit global prohibited under strict mode!!!!
 
 //TODO String Interpolation VS Concatenation
-
+// console.log("I am Matteo, and my favorite food is " + favoriteFood)
+// console.log(`I am Matteo, and my favorite food is ${favoriteFood}`)
 
 //TODO Conditional Statements
 // 1. if/else if/else
+// if (favoriteFood === "lasagna") {
+//     console.log("This must be an Italian!")
+// } else if (favoriteFood === "mac & cheese") {
+//     console.log("This must be an American!")
+// } else {
+//     console.log("ORIGIN UNKNOWN")
+// }
 
 // 2. Switch Statements
+// switch (favoriteFood) {
+//     case "lasagna":
+//         console.log("This must be an Italian!")
+//         break;
+//     case "mac & cheese":
+//         console.log("This must be an American!")
+//         break;
 
+//     default:
+//         console.log("ORIGIN UNKNOWN")
+        // break;
+// }
 //TODO Ternary Expressions
 // condition ? do something if truthy : do something if falsey
+favoriteFood === "lasagna" ? console.log("You are Italian!") : console.log("You are not Italian")
 
 //TODO Loops
 // 1. While
--- 00_JS_Fundamentals/notes.md
+++ 00_JS_Fundamentals/notes.md
@@ -4,3 +4,7 @@
 - nearly 90% of all websites use JS (and is understood by every browser)
 - DOM manipulation abilities
 - ECMAScript handles the official maintenance of the language (i.e. ES6)
+- Case sensitive when it comes to name variable, functions, etc
+- Often used in tandem with HTML, CSS
+- Dynamic language, not statically typed (TypeScript)
+- Weakly typed language -> implicit type conversions 

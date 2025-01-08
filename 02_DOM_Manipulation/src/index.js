2d15a4f6b4ef38e185f1afb59d2a4ac3062dae29
Matteo Piccini <gnappo.1@live.it>
2025-01-08 10:03:23 -0800

    add base code for lecture

---
+++ 02_DOM_Manipulation/src/index.js
@@ -0,0 +1,62 @@
+//* BookStore has been moved to data.js
+console.log(bookStore);
+
+//* Code from previous lecture
+
+function formatPrice(price) {
+  return '$' + parseFloat(price).toFixed(2);
+}
+
+//* New code starts here
+
+//* Concepts
+//! DOM Targeting
+
+//! Single Element
+//* querySelector => 
+//* getElementById =>
+//! Multiple elements
+//* querySelectorAll => 
+//* getElementByClassName =>
+//* getElementByTagName =>
+
+//! Node Properties
+//* innerText => 
+//* textContent =>
+//* innerHTML =>
+//* className =>
+//* classList =>
+//* id => 
+
+//! Node Relationship Properties
+//* children
+//* lastChild
+//* firstChild
+//* parentNode / parentElement
+//* previousSibling / previousElementSibling
+//* nextSibling / nextElementSibling
+
+//! Create Retrieve Update Destroy (CRUD) Methods
+//* createElement('tag')
+//* prepend(elements)
+//* append(elements)
+//* appendChild(newElement)
+//* removeChild(childElement)
+//* insetAdjacentElement(position, newElement)
+
+// TODO CRUD
+
+// TODO 💡 Create a new DOM element of your choice and add it to the page
+
+// TODO 💡 Create a function that sets the text content of the header to the bookstore name.
+
+// TODO 💡 Create a function that grabs all the divs from the footer and render the bookstore name, address, and hours
+
+// TODO 💡 Remove an element of your choice from the DOM
+
+
+// TODO 💡 Exercise After Break
+// create a function called renderBook(book)
+// it will take a book object as an argument
+// and create the html structure for rendering 
+// that book and insert it into our webpage!

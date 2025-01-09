4e633e2fd1d6de794b3b821bf34a7fae5e6125e1
Matteo Piccini <gnappo.1@live.it>
2025-01-09 12:16:22 -0800

    end of lecture code

--- 03_DOM_Events/src/index.js
+++ 03_DOM_Events/src/index.js
@@ -48,6 +48,7 @@ function renderBook(book) {
     img.alt = book.title
     const button = document.createElement("button")
     button.innerText = "Delete"
+    button.addEventListener("click", (e) => e.target.parentNode.remove())
     li.append(h3, pAuthor, pPrice, img, button)
     // figure out where
     // target that place with querySelector/getElementById
@@ -55,6 +56,7 @@ function renderBook(book) {
     // append
     ulList.appendChild(li)
 }
+
 function renderBookAsHTML(book) {
     const ulList = document.getElementById("book-list")
     ulList.innerHTML += `
@@ -63,14 +65,18 @@ function renderBookAsHTML(book) {
         <p>${book.author}</p>
         <p>${formatPrice(book.price)}</p>
         <img src=${book.imageUrl} alt=${book.title}/>
-        <button class="delete-btn">Delete</button>
+        <button data-id=${book.id} class="delete-btn">Delete</button>
     </li>
     `
+    // const deleteBtn = document.querySelector(`li.list-li button[data-id="${book.id}"]`)
+    // deleteBtn.addEventListener("click", (e) => e.target.parentNode.remove())
 }
+
 
 setHeader()
 changeFooter()
-bookStore.inventory.forEach(bookObj => renderBookAsHTML(bookObj))
+bookStore.inventory.forEach(bookObj => renderBook(bookObj))
+document.querySelectorAll("li.list-li button").forEach(btn => btn.addEventListener("click", (e) => e.target.parentNode.remove()))
 // bookStore.inventory.forEach(renderBook) this line leverages JS magic 
 // BUT IT'S IDENTICAL TO THE ONE ABOVE
 
@@ -85,7 +91,32 @@ bookStore.inventory.forEach(bookObj => renderBookAsHTML(bookObj))
 //! Suggestions When Working With the DOM
 
 //TODO 1. Set global selector variables at the top of the file for everyone to use
+const toggleButton = document.querySelector("#toggleForm")
+const bookForm = document.querySelector("#book-form")
+// const handleClickToggle = () => {}
 //TODO 2. Attach event listeners to the correct DOM nodes
+// toggleButton.addEventListener("click", (e) => bookForm.classList.toggle("collapsed"))
+toggleButton.addEventListener("click", (e) => e.target.nextElementSibling.classList.toggle("collapsed"))
+
+bookForm.addEventListener("submit", (e) => {
+    //! The first step is always to PREVENT PAGE REFRESHES!!!!!
+    e.preventDefault()
+    //! Extract the data out of the form and create a new object to contain the data!!
+    const newBook = {
+        title: document.querySelector("#form-title").value,
+        author: document.querySelector("#form-author").value,
+        price: parseFloat(document.querySelector("#form-price").value) || 0,
+        imageUrl: document.querySelector("#form-imageUrl").value,
+        inventory: parseInt(document.querySelector("#form-inventory").value) || 0,
+    }
+    //! VALIDATE THE INFORMATION BEFORE USING IT!!!!
+    
+    //! Use the book however you see fit!
+    renderBook(newBook)
+    
+    //! ON SUCCESS, reset the form
+    e.target.reset()
+})
 //TODO 3. Decide if creating the callback anonymously in-place OR pass a function reference (promotes reusability)
 //TODO 4. Does the callback have access to all the data it needs or should it receive parameters?
 
